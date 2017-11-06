88ff1bf9bea517d4bcffe40199f3fd30f4d9739d
Marcel Schnelle <aurae@web.de>
2017-11-06 21:20:13 +0900

    Hello Application!

---
+++ app/src/test/java/de/mannodermaus/kommandah/test/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package de.mannodermaus.kommandah.test
+
+import org.junit.Assert.*
+import org.junit.jupiter.api.Test
+
+class ExampleUnitTest {
+  @Test
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+  }
+}
